cc6a8e909c188efbe8fceb571e414b9175ad5d3a
Michael Passmore <mpassmore14@yahoo.com>
2025-06-26 13:35:45 -0500

    Fixed bugs 1 and 2

--- capstone-starter/src/main/java/org/yearup/EasyshopApplication.java
+++ capstone-starter/src/main/java/org/yearup/EasyshopApplication.java
@@ -9,6 +9,7 @@ public class EasyshopApplication
 
     public static void main(String[] args) {
         SpringApplication.run(EasyshopApplication.class, args);
+        System.out.println("http://localhost:63343/capstone-starter/capstone-client-web-application/index.html?_ijt=qh09m9adsdvd21bguh3jc6lubr&_ij_reload=RELOAD_ON_SAVE");
     }
 
 }
